6fe219e1704fb4c805d577e2c44c3942d3586998
thomasdunlap <tom.a.dunlap@gmail.com>
2017-11-09 20:25:58 -0500

    Added get_dict() to advanced_python_csv.py.

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,15 @@
 PLACE YOUR CODE HERE
+
+import csv
+
+def get_dict():
+    d = {}
+    reader = csv.reader(open('faculty.csv'))
+    for row in reader:
+        key = row[0].split()[-1]
+        if key not in d:
+            d[key] = [row[1:]]
+        else:
+            d[key].append(row[1:])
+    del d['name']
+    return d
