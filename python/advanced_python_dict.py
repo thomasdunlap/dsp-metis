9e45575c467db25e5050a4b29da4186dc82a9933
thomasdunlap <tom.a.dunlap@gmail.com>
2017-11-25 22:42:12 -0500

    Added functions to advanced python files.

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,33 @@
-PLACE YOUR CODE HERE
+import csv
+
+def get_dict():
+    '''
+    d = {}
+    reader = csv.reader(open('faculty.csv'))
+    for row in reader:
+        key = tuple(row[0].split(' '))
+        d[key] = row[1:]
+    del d[('name',)]
+    return d
+    '''
+    with open('faculty.csv') as f:
+        reader = csv.reader(f)
+        # Dictionary (tuple of name):(rest of row) for each entry
+        d = {tuple(row[0].split(' ')): row[1:] for row in reader}
+        del d[('name',)]
+        return d
+
+import csv
+
+def get_dict():
+    d = {}
+    with open('faculty.csv') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            key = row[0].split()[-1]
+            if key not in d:
+                d[key] = [row[1:]]
+            else:
+                d[key].append(row[1:])
+    del d['name']
+    return d
--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,49 @@
-PLACE YOUR CODE HERE
+import csv
+import re
+
+def count_degrees(csv_file_name):
+    degrees = {}
+    with open(csv_file_name) as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            ds = row[1].split()
+
+            for i in range(len(ds)):
+                ds[i] = re.sub('\.', '', ds[i])
+
+                ds_up = ds[i].upper()
+                if ds_up not in degrees:
+                    degrees[ds_up] = 1
+                else:
+                    degrees[ds_up] += 1
+
+    del degrees['DEGREE']
+    return degrees
+
+
+import csv
+
+def count_titles(csv_file_name):
+    titles = {}
+    with open(csv_file_name) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            title = line[2].split(' ')[0].lower()
+            if title not in titles:
+                titles[title] = 1
+            else:
+                titles[title] += 1
+
+    del titles['']
+    return titles
+
+
+import csv
+
+def emails(csv_file_name):
+    with open(csv_file_name) as csvfile:
+        reader = csv.reader(csvfile)
+        return [row[3] for row in reader][1:]
+
+def unique_domains(emails):
+    return set(re.split('@', e)[1] for e in emails)
